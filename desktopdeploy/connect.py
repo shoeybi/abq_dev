2ef9908d85c050e1472248781eccc8cb71bee57e
Yaser Khalighi <khalighi@Yasers-MacBook-Pro.local>
2013-06-30 14:59:11 -0700

    connect.py is updated

--- desktopdeploy/connect.py
+++ desktopdeploy/connect.py
@@ -1,16 +1,35 @@
 import boto 
 import ec2
-def AWSquery():
-    regions = boto.ec2.regions()
-    for region in regions:
+# a key
+key = 'AWS_desktop_hosts'
+# a security group
+scg = ['quicklaunch-0']
+# an AMI
+ami = 'ami-fe002cbb'
+# supported instance types
+INSTANCE_TYPES = {'micro':'t1.micro', 
+                  'small' :'m1.small',
+                  'medium':'m1.medium',
+                  'large ':'m1.large',
+                  }
+# AWSquery shows all the instances that are currently running
+def query(instances=None,regions="all"):
+# loop through all regions   
+    regions_ = boto.ec2.regions()
+    for region in regions_:
+# only connect to the regions listed
+        if region.name not in regions and regions is not "all":
+            continue
         conn = region.connect()
-        reservations 	= conn.get_all_instances()
-#    images = conn.get_all_images()
-#    for image in images:
+        reservations 	= conn.get_all_instances(instances)
+# loop through all reservations in a region
         for reservation in reservations:
-            instances	= reservation.instances
-            for instance in instances :
+            instances_	= reservation.instances
+# loop through all instances in a reservation
+            for instance in instances_ :
+# update the instance
                 instance.update()
+# print all the data
                 print "-------------------------------------------------------"
                 print "id   	   ", instance.id
                 print "type        ", instance.instance_type
@@ -23,15 +42,101 @@ def AWSquery():
                 print "key name    ", instance.key_name
                 print "image_id    ", instance.image_id
 
-def AWSrun(region='us-west-1',type='micro'):
+def launch_instance(type='micro',region='us-west-1'):
+# connect to the region
     conn = boto.ec2.connect_to_region(region)
-    key = 'AWS_desktop_hosts'
-    if ( type == 'micro' ) :
-        conn.run_instances('ami-fe002cbb',instance_type='t1.micro',key_name=key)
-    if ( type == 'small' ) :
-        conn.run_instances('ami-fe002cbb',instance_type='m1.small',key_name=key)
-    if ( type == 'medium' ) :
-        conn.run_instances('ami-fe002cbb',instance_type='m1.medium',key_name=key)
-    if ( type == 'large' ) :
-        conn.run_instances('ami-fe002cbb',instance_type='m1.large',key_name=key)
-        
+    if conn is None :
+        raise NameError("region "+region+" is invalid")
+# get the instance type based on type
+    try:
+        instance_type = INSTANCE_TYPES[type]
+    except:
+        print "instance type "+type+" is not supported"
+        raise
+# launch the instance
+    instance = conn.run_instances(ami,
+                                  instance_type=instance_type,
+                                  security_groups=scg,
+                                  key_name=key).instances[0]
+    if instance is None :
+        raise NameError("instance could not be launched")
+# return the id and regionn
+    return instance.id,region
+
+def get_instance(instanceId,region='us-west-1'):
+
+# get the connection to the region
+    conn     = boto.ec2.connect_to_region(region)
+    if conn is None :
+        raise NameError("region "+region+" is invalid")
+
+# get the instance 
+    try:
+        instance = conn.get_all_instances(instance_ids=instanceId)[0].instances[0]
+    except (boto.exception.EC2ResponseError,IndexError):
+        raise NameError("instance id "+instanceId+" was not found")
+
+# return the instance
+    return instance 
+
+def stop_instance(instanceId,region='us-west-1'):
+# get connection    
+    conn     = boto.ec2.connect_to_region(region)
+    if conn is None :
+        raise NameError("region "+region+" is invalid")
+
+# stop the instance
+    try:
+        result = conn.stop_instances(instance_ids=instanceId)
+    except boto.exception.EC2ResponseError:
+        raise NameError("could not stop instance "+instanceId)
+
+# check if it is stopped
+    if (result[0].id != instanceId):
+        raise NameError("could not stop instance "+instanceId)
+
+def terminate_instance(instanceId,region='us-west-1'):
+# get connection    
+    conn     = boto.ec2.connect_to_region(region)
+    if conn is None :
+        raise NameError("region "+region+" is invalid")
+
+# terminate the instance
+    try:
+        result = conn.terminate_instances(instance_ids=instanceId)
+    except boto.exception.EC2ResponseError:
+        raise NameError("could not terminate instance "+instanceId)
+
+# check if it is terminated
+    if (result[0].id != instanceId):
+        raise NameError("could not terminate instance "+instanceId)
+
+def start_instance(instanceId,region='us-west-1'):
+# get connection    
+    conn     = boto.ec2.connect_to_region(region)
+    if conn is None :
+        raise NameError("region "+region+" is invalid")
+
+# start the instance    
+    try:
+        result = conn.start_instances(instance_ids=instanceId)
+    except boto.exception.EC2ResponseError:
+        raise NameError("could not start instance "+instanceId)
+
+# check if it is started
+    if (result[0].id != instanceId):
+        raise NameError("could not start instance "+instanceId)
+
+def state_instance(instanceId,region='us-west-1'):
+# get connection
+    try:
+        instance = get_instance(instanceId,region=region)
+        return instance.state
+    except NameError:
+        return "invalid"
+    
+def run_at_instance(instanceId,region='us-west-1'):
+# get instance
+    instance = get_instance(instanceId,region=region)
+    
+    if instance.state
--- desktopdeploy/test.py
+++ desktopdeploy/test.py
@@ -1,5 +1,12 @@
-import connect
-connect.AWSrun(region='us-west-1',type='micro')
-connect.AWSquery()
+#import connect
+import subprocess
 #connect.AWSquery2('micro')
 #connect.AWSquery2("us-west-1")
+p = subprocess.Popen('sleep 3', 
+                     shell=True, 
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.STDOUT)
+for line in p.stdout.readlines():
+    print line,
+retval = p.wait()
+
