143d2c83124ab2e8058550fd79a4d12ae93cf44a
Yaser Khalighi <khalighi@Yasers-MacBook-Pro.local>
2013-07-04 10:52:02 -0700

    connect.py is updated

--- desktopdeploy/connect.py
+++ desktopdeploy/connect.py
@@ -1,7 +1,10 @@
 import boto 
 import ec2
+import time
+import sys
+import subprocess
 # a key
-key = 'AWS_desktop_hosts'
+key_dir = '/Users/khalighi/Projects/Abaqual/keys'
 # a security group
 scg = ['quicklaunch-0']
 # an AMI
@@ -42,7 +45,25 @@ def query(instances=None,regions="all"):
                 print "key name    ", instance.key_name
                 print "image_id    ", instance.image_id
 
-def launch_instance(type='micro',region='us-west-1'):
+# kills all the instances that are currently available
+def terminate_all(instances=None,regions="all"):
+# loop through all regions   
+    regions_ = boto.ec2.regions()
+    for region in regions_:
+# only connect to the regions listed
+        if region.name not in regions and regions is not "all":
+            continue
+        conn = region.connect()
+        reservations 	= conn.get_all_instances(instances)
+# loop through all reservations in a region
+        for reservation in reservations:
+            instances_	= reservation.instances
+# loop through all instances in a reservation
+            for instance in instances_ :
+# update the instance
+                instance.terminate()
+
+def launch_instance(type='micro',key_name='abaqual_key',region='us-west-1'):
 # connect to the region
     conn = boto.ec2.connect_to_region(region)
     if conn is None :
@@ -53,11 +74,25 @@ def launch_instance(type='micro',region='us-west-1'):
     except:
         print "instance type "+type+" is not supported"
         raise
+# create the key if needed
+    if conn.get_key_pair(key_name) is None:
+        key      = conn.create_key_pair(key_name)
+        try:
+            key.save( key_dir )
+        except:
+            raise NameError("could not save the key "+key_name)
+    else:
+        filename = key_dir+"/"+key_name+'.pem'
+        try:
+            with open(filename): pass
+        except IOError:
+            raise NameError("cannot find "+filename)
+        
 # launch the instance
     instance = conn.run_instances(ami,
                                   instance_type=instance_type,
                                   security_groups=scg,
-                                  key_name=key).instances[0]
+                                  key_name=key_name).instances[0]
     if instance is None :
         raise NameError("instance could not be launched")
 # return the id and regionn
@@ -131,12 +166,80 @@ def state_instance(instanceId,region='us-west-1'):
 # get connection
     try:
         instance = get_instance(instanceId,region=region)
+        instance.update()
         return instance.state
     except NameError:
         return "invalid"
+
+def instance_is_running(instanceId,region='us-west-1'):
     
-def run_at_instance(instanceId,region='us-west-1'):
-# get instance
+    i = 0
+    while i < 60 :
+        state = state_instance(instanceId,region=region)
+        i = i + 1
+        if (state == "pending"):
+            if i == 1:
+                print "waiting for instance "+instanceId+" to get ready",
+            print ".",
+            sys.stdout.flush()
+            time.sleep(5)
+        elif (state == "running"):
+            if i > 1:
+                print "running! waiting for another 30 sec"
+                time.sleep(30)
+            return 1
+        else:
+            print ""
+            return 0
+        
+    print ""
+    return 0
+
+def run_at_instance(instanceId,input,command_type='command',wait=False,region='us-west-1',verbose=0):
+# get connection
+    conn     = boto.ec2.connect_to_region(region)
+    if conn is None :
+        raise NameError("region "+region+" is invalid")
+#get instance and check if it is running    
     instance = get_instance(instanceId,region=region)
-    
-    if instance.state
+    if not instance_is_running(instanceId,region):
+        raise NameError("cannot connect to instance "+instanceId)
+#get key and ip address
+    instance.update()
+    public_dns    = instance.public_dns_name
+    key_name      = instance.key_name
+    key_filename = key_dir+"/"+key_name+'.pem'
+    try:
+        with open(key_filename): pass
+    except IOError:
+        raise NameError("cannot find "+filename)
+#prepare the input command
+    if   command_type is 'command' :
+        command_prep = ' "'+input+'"'
+    elif command_type is 'script' :
+        try:
+            with open(input): pass
+        except IOError:
+            raise NameError("cannot find "+input)
+        command_prep = ' <'+input
+    else :
+        raise nameError(command_type+" is not recognized")
+#prepare the ssh command
+    ssh_command = 'ssh  -o "GSSAPIAuthentication no" -o "StrictHostKeyChecking no" -i '+key_filename+' ubuntu@'+public_dns
+#run the command
+    if verbose > 0: 
+        print "running at instance : "+ssh_command+command_prep  
+    p = subprocess.Popen(ssh_command+command_prep,
+                         shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+#write the output
+    if verbose > 1: 
+        for line in p.stdout.readlines():
+            print line,
+#wait for the command to finish
+    if wait:
+        for line in p.stdout.readlines():
+            pass
+#return the output
+    return (p.stdout,p.stderr)
--- desktopdeploy/test.py
+++ desktopdeploy/test.py
@@ -1,12 +1,25 @@
-#import connect
-import subprocess
+import ec2
+import boto
+conn     = boto.ec2.connect_to_region('us-west-1')
+print conn.get_all_key_pairs()
+#key      = conn.create_key_pair('mynewkey3')
+keyPair2  = conn.get_key_pair('mynewkey5')
+print keyPair2
+#help(keyPair2)
+#keyPair2.save('/tmp')
+
+#import subprocess
 #connect.AWSquery2('micro')
 #connect.AWSquery2("us-west-1")
-p = subprocess.Popen('sleep 3', 
-                     shell=True, 
-                     stdout=subprocess.PIPE, 
-                     stderr=subprocess.STDOUT)
-for line in p.stdout.readlines():
-    print line,
-retval = p.wait()
+#p = subprocess.Popen('ls -altr', 
+#                     shell=True, 
+#                     stdout=subprocess.PIPE, 
+#                     stderr=subprocess.STDOUT)
+#for line in p.stdout.readlines():
+#    print line,
+#retval = p.wait()
 
+#if 0 :
+#    print "haha"
+#if 1 :
+#    print "hoojp"
