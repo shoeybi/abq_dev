5a33dd26902dc273812a535b13639b44ca7347a9
Yaser Khalighi <yaser@abaqual.com>
2013-10-26 10:28:23 -0700

    testInterface

--- desktopdeploy/testInterface.py
+++ desktopdeploy/testInterface.py
@@ -1,8 +1,8 @@
 import interface
-company_name = 'lala'
+company_name = 'lala2'
 supported_regions = ['us-west-1','us-west-2','us-east-1']
 interface.make_company(company_name, supported_regions) 
-id1 = interface.get_instance_id('us-west-1', 'm1.small', 'ubuntu12.04',company_name, 'ybro', '1534a48d06f8cd7322a1c0725ae437826324dc8f')
+id1 = interface.get_instance_id('us-west-1', 'm1.small', 'ubuntu12.04',company_name, 'ykhalighi', '880ecc95a92e47faf13b70c175c9733364f52fca')
 #interface.terminate_instance(id ,region) 
 print id1
 
