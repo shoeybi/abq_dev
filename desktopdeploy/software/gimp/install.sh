9887b6c4f08288ee0ce1b44328ee30b50e726881
Yaser Khalighi <yaser@abaqual.com>
2014-01-16 00:13:07 -0800

    small fix

---
+++ desktopdeploy/software/gimp/install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+## script to install gimp
+
+sudo -E apt-get update  
+sudo -E apt-get --force-yes -y install gimp
+
---
+++ desktopdeploy/software/gimp/uninstall.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+## script to install octave 
+
+sudo -E apt-get --force-yes -y purge gimp
